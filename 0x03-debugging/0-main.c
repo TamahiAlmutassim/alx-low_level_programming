9ecce1e724ca9023fde210c9e5df1a5fc352462e
Tamahi Almutassim <tamahee333@gmail.com>
2023-09-14 07:44:05 -0700

    wai

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,22 @@
 #include "main.h"
+
 /**
-* main - Test function for positive or negative
-* Return: 0
-*/
+ * positive_or_negative - Determines whether an integer is positive, negative, or zero.
+ * @n: The integer to check.
+ *
+ * Return: No return value (void).
+ */
+void positive_or_negative(int n);
+
+/**
+ * main - Test function for positive_or_negative.
+ * Return: 0.
+ */
 int main(void)
 {
-int i;
-i = 0;
-positive_or_negative(i);
-return (0);
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
 }
