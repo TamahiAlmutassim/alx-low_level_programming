6f4e71a5867eda98de04723d6b25f3ee05f61384
Tamahi Almutassim <Tamahee333@gmail.com>
2023-11-15 08:37:40 -0800

    recursion task

---
+++ 0x08-recursion/main.c
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void _puts_recursion(char *k);
+int _putchar(char c);
+void _print_rev_recursion(char *k);
+int _strlen_recursion(char *k);
+int factorial(int k);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int k);
+int is_prime_number(int k);
+int is_palindrome(char *k);
+#endif
